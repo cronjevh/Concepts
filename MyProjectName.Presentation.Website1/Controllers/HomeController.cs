f68dbeb69ec8c8b8aa6fbfab7e30b8ac9b4ad7e5
Cronje van Heerden <cronjeVH@CCHP.onmicrosoft.com>
2018-05-05 14:18:48 +0200

    Demo

---
+++ MyProjectName.Presentation.Website1/Controllers/HomeController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyProjectName.Presentation.Website1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index() => View();
+    }
+}
