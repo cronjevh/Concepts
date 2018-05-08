7ad4f51b7c883f4c97e3bb34b50eee3ed475b830
Cronje van Heerden <cronjeVH@CCHP.onmicrosoft.com>
2018-05-08 21:39:42 +0200

    Meteor Todos

--- MyProjectName.Presentation.Website1/Controllers/HomeController.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Mvc;
-
-namespace MyProjectName.Presentation.Website1.Controllers
-{
-    public class HomeController : Controller
-    {
-        public IActionResult Index() => View();
-    }
-}
